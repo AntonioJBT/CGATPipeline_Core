c8341d217311494bad93922eed05b8252082075d
AntonioJBT <a.berlanga@imperial.ac.uk>
2017-04-27 19:31:53 +0100

    updates/testing

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ from __future__ import unicode_literals
 from __future__ import division
 from __future__ import absolute_import
 
-from builtins import open
+#from builtins import open
 from builtins import str
 from future import standard_library
 standard_library.install_aliases()
